ae110576da320757697abd58341152447e57baf2
Dio <diooktar@gmail.com>
2021-01-28 00:09:43 +0700

    Struktur

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <>
+      <p>Ini Main</p>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <p>Ini Navbar</p>
+    </>
+  );
+};
+
+export default Navbar;
