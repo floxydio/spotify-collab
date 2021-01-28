54ac5dfc0e0406f32a736395738d1cfb1c1c6c2b
Dio <diooktar@gmail.com>
2021-01-28 14:39:26 +0700

    Navbar belom responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,22 @@
 import React from "react";
+import Logo from "../../images/spotify.png";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <>
-      <div className="container">
-        <p>Ini Navbar</p>
+      <div className="container bg-dark text-light">
+        <img src={Logo} alt="Navbar" width="140" />
+        <div className="text-right">
+          <ul>
+            <li>Premium</li>
+            <li>Dukungan</li>
+            <li>Download</li>
+            <span className="garis-nav">|</span>
+            <li>Daftar</li>
+            <li>Masuk</li>
+          </ul>
+        </div>
       </div>
     </>
   );
