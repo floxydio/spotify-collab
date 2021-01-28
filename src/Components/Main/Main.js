424abf0c91429b6136d9d2f3b0ad41114696359e
unknown <mada.lawliet@gmail.com>
2021-01-28 20:34:36 +0700

    Update on Navbar and Main

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./Main.css";
 
 const Main = () => {
   return (
     <>
-      <p>Ini Main</p>
+      <section className="main">
+        <div className="container">
+          <div className="tagline-wrapper">
+            <div className="tagline-top">
+              <h1>
+                Listening is <span class="tagline-right">everything</span>
+              </h1>
+            </div>
+            <div className="tagline-bottom">
+              <h4>Jutaan lagu dan podcast. Tanpa kartu kredit.</h4>
+              <button>
+                <a href="#">DAPATKAN SPOTIFY FREE</a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,16 +6,18 @@ const Navbar = () => {
   return (
     <>
       <div className="container bg-dark text-light">
-        <img src={Logo} alt="Navbar" width="140" />
-        <div className="text-right">
-          <ul>
-            <li>Premium</li>
-            <li>Dukungan</li>
-            <li>Download</li>
-            <span className="garis-nav">|</span>
-            <li>Daftar</li>
-            <li>Masuk</li>
-          </ul>
+        <div className="navbar">
+          <img src={Logo} alt="Navbar" width="140" />
+          <div className="text-right">
+            <ul>
+              <li>Premium</li>
+              <li>Dukungan</li>
+              <li>Download</li>
+              <span className="garis-nav">|</span>
+              <li>Daftar</li>
+              <li>Masuk</li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
