b6892750c15bb8cc682f89eb0360965f3a69ee81
Dio <diooktar@gmail.com>
2021-01-29 02:32:07 +0700

    Footer Kudu difix

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "./Footer.css";
+import logo from "../../images/spotify.png";
 
 const Footer = () => {
   return (
-    <>
-      <p>ini Footer</p>
-    </>
+    <footer className="container bg-darks pt-8">
+      <img src={logo} alt="Footer" width="140" className="footer-image" />
+      <div className="text-footer">
+        <i className="fab fa-instagram fa-lg"></i>
+        <i className="fab fa-twitter fa-lg"></i>
+        <i className="fab fa-facebook fa-lg"></i>
+      </div>
+      <div className="text-footer-center">
+        <p className="heading">Perusahaan</p>
+      </div>
+    </footer>
   );
 };
 
